572cac3827994723c5f3c32ac891f1526dbfbef7
Your Name <you@example.com>
2020-05-13 14:50:14 -0500

    Updated2

--- TAREAS/src/EjerciciO1.java
+++ TAREAS/src/EjerciciO1.java
@@ -1,27 +1,25 @@
 import java.util.Scanner;
 
 public class EjerciciO1 {
-	
+
 	static int fecha1, fecha2, fechare;
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner (System.in);
-		
+		Scanner scan = new Scanner(System.in);
+
 		System.out.println("Ingrese la primera fecha: ");
 		fecha1 = scan.nextInt();
-		
-		
+
 		if (fecha1 <= 30) {
-			
+
 			System.out.println("Ingrese la segunda fecha: ");
 			fecha2 = scan.nextInt();
-			
-			fechare = fecha1 - fecha2;
-			
-			System.out.println("La diferencia de ambas fechas es : "+ fechare + " dias");
-			
-		}
 
+			fechare = fecha1 - fecha2;
+
+			System.out.println("La diferencia de ambas fechas es : " + fechare + " dias");
+
+		}
 
 	}
 
