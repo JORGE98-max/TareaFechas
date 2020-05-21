dfbe7d162414f6e5942a29f827a22746e386639a
Your Name <you@example.com>
2020-05-21 14:32:10 -0500

    Updated

---
+++ TAREAS/src/EjercicioWhile.java
@@ -0,0 +1,19 @@
+
+public class EjercicioWhile {
+
+	public static void main(String[] args) {
+		
+		int num = 1;
+		
+		while (num <10 && num!=7) {
+			
+			
+		System.out.println(num);
+		num+=2;
+		}
+	
+		System.out.println("9");
+
+	}
+
+}
